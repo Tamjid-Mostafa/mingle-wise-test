de2bc371ea3e3cafab38963c91d8c1d0658885be
hardikvij195 <62573477+hardikvij195@users.noreply.github.com>
2023-04-06 23:32:45 +0530

    updated website

--- .firebase/minglewise/functions/server.js
+++ .firebase/minglewise/functions/server.js
@@ -1,3 +1,4 @@
 const { onRequest } = require('firebase-functions/v2/https');
-const server = import('firebase-frameworks');
-exports.ssr = onRequest((req, res) => server.then(it => it.handle(req, res)));
+  const server = import('firebase-frameworks');
+  exports.ssrminglewise = onRequest({}, (req, res) => server.then(it => it.handle(req, res)));
+  
