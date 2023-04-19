4d82e0b414277a9945440deb26ba440e98701b44
Bayezid Mostafa <hiperx1@gmail.com>
2023-04-19 20:27:06 +0600

    Meta pixel and g-tag updated

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,8 @@ import { createContext, useEffect, useState } from "react";
 import TagManager from "react-gtm-module";
 import { Toaster } from "react-hot-toast";
 export const StateContext = createContext();
+import * as fbq from "../lib/fpixel";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   const [osName, setOsName] = useState("");
@@ -23,26 +25,23 @@ export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init(process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID); // facebookPixelId
-        ReactPixel.pageView();
-
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
+    fbq.pageview();
+    const handleRouterChange = () => {
+      fbq.pageview();
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
   }, [router.events]);
 
   useEffect(() => {
-    TagManager.initialize({ gtmId: `${process.env.NEXT_PUBLIC_GTM_ID}` });
+    TagManager.initialize({ gtmId: "G-1GDKXS7R5W" });
   }, []);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(true);
-      // console.log("Loading");
     }, 2000);
   }, []);
 
@@ -55,6 +54,37 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head />
+      <>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-1GDKXS7R5W`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1GDKXS7R5W');
+          `}
+        </Script>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+          }}
+        />
+      </>
       {!loading ? (
         <>
           <Loader />
